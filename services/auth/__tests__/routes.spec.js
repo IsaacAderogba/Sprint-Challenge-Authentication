5d960c810943965fc1fb15c5187101e589fdabba
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-26 09:23:02 +0100

    Add test for user login

--- services/auth/__tests__/routes.spec.js
+++ services/auth/__tests__/routes.spec.js
@@ -7,7 +7,8 @@ beforeEach(async () => {
   await db("users").truncate();
 });
 
-describe(" POST] /api/auth/register | Register user route handler", () => {
+describe("[POST] /api/auth/register | Register user route handler", () => {
+
   it("returns the details of the registered user", () => {
     return request(server)
       .post("/api/auth/register")
@@ -37,4 +38,36 @@ describe(" POST] /api/auth/register | Register user route handler", () => {
         expect(users).toHaveLength(1);
       });
   });
+
+});
+
+describe("[POST] /api/auth/login | Login user route handler", () => {
+
+  beforeEach(async () => {
+    // register the user
+    return request(server)
+      .post("/api/auth/register")
+      .send({
+        username: "Isaac",
+        password: "12345",
+      })
+      .expect(201);
+  });
+
+  it('logs the user in given a username and password', () => {
+    return request(server)
+      .post("/api/auth/login")
+      .send({
+        username: "Isaac",
+        password: "12345",
+      })
+      .expect(200)
+      .then(res => {
+        const { user } = res.body;
+
+        expect(user.username).toEqual("Isaac");
+        expect(res.body).toHaveProperty("token");
+      });
+  });
+
 });
