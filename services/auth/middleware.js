413d207f9dfcf4887418d8554288f7e73df539c1
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-26 08:37:24 +0100

    Add validateId middleware

--- services/auth/middleware.js
+++ services/auth/middleware.js
@@ -1,3 +1,4 @@
+/* eslint-disable require-atomic-updates */
 const jwt = require("jsonwebtoken");
 const Controller = require("./controllers");
 
@@ -13,6 +14,28 @@ module.exports = {
 
     req.user = req.body;
     next();
+  },
+  validateId: async function(req, res, next) {
+    const { id } = req.params;
+
+    if (Number.isInteger(parseInt(id, 10))) {
+      try {
+        const user = await Controller.getUserById(id);
+
+        if (user) {
+          req.user = user;
+          next();
+        } else {
+          res
+            .status(404)
+            .json({ message: `The user with Id of '${id}' does not exist` });
+        }
+      } catch (err) {
+        next(err);
+      }
+    } else {
+      res.status(400).json({ message: `The Id of '${id}' is not valid` });
+    }
   },
   authenticateUser: function(req, res, next) {
     const token = req.get("Authorization");
