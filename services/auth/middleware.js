338fd242c3ce44ad983b08e922c20df58600a7b6
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-26 08:34:14 +0100

    Add authenticate user middleware

--- services/auth/middleware.js
+++ services/auth/middleware.js
@@ -1,5 +1,5 @@
-const jwt = require('jsonwebtoken');
-const Controller = require('./controllers');
+const jwt = require("jsonwebtoken");
+const Controller = require("./controllers");
 
 module.exports = {
   validateBody: function(req, res, next) {
@@ -10,8 +10,25 @@ module.exports = {
         .status(400)
         .json({ message: "Missing required fields username or password" });
     }
-    
+
     req.user = req.body;
     next();
+  },
+  authenticateUser: function(req, res, next) {
+    const token = req.get("Authorization");
+
+    if (token) {
+      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+        if (err) return res.status(401).json(err);
+
+        req.decodedToken = decoded;
+
+        next();
+      });
+    } else {
+      return res.status(401).json({
+        error: "No token provided, must be set on the Authorization Header"
+      });
+    }
   }
-}
+};
