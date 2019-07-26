6d24897b6c752ddc0d5e4af1789d744c7a9356ec
Isaac Aderogba <isaacaderogba1@gmail.com>
2019-07-26 10:08:16 +0100

    Create client side app

--- server/api/server.js
+++ server/api/server.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 const helmet = require('helmet');
 const api = require('../services');
 
-// const configureRoutes = require('../config/routes.js');
-
 const server = express();
 
 server.use(helmet());
@@ -13,7 +11,5 @@ server.use(cors());
 server.use(express.json());
 
 server.use(api);
-
-// configureRoutes(server);
 
 module.exports = server;
